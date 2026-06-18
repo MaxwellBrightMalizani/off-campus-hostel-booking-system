5d44b4b28200765ae22787c6b42322fa6e42fc63
Lauritta <laurittablake@mail.com>
2026-06-18 11:08:46 +0200

    git commit -m "feat(accounts): add CustomUser model, auth views, decorators, templates, and tests"

--- system/system/urls.py
+++ system/system/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('system.Accounts.urls')),
     path('', include('listings.urls')),
 ]
